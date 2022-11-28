3bed0996efd82c858a0b52a6bd6c9cf30aa05965
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-11-27 23:44:30 -0300

    LargeCard

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
+import banner2 from "../assets/img/banner2.png";
 
 const exploreData = [{"img":"https://links.papareact.com/5j2","location":"London","distance":"45-minute drive"},{"img":"https://links.papareact.com/1to","location":"Manchester","distance":"4.5-hour drive"},{"img":"https://links.papareact.com/40m","location":"Liverpool","distance":"4.5-hour drive"},{"img":"https://links.papareact.com/msp","location":"York","distance":"4-hour drive"},{"img":"https://links.papareact.com/2k3","location":"Cardiff","distance":"45-minute drive"},{"img":"https://links.papareact.com/ynx","location":"Birkenhead","distance":"4.5-hour drive"},{"img":"https://links.papareact.com/kji","location":"Newquay","distance":"6-hour drive"},{"img":"https://links.papareact.com/41m","location":"Hove","distance":"2-hour drive"}];
 const cardData = [{"img":"https://links.papareact.com/2io","title":"Outdoor getaways"},{"img":"https://links.papareact.com/q7j","title":"Unique stays"},{"img":"https://links.papareact.com/s03","title":"Entire homes"},{"img":"https://links.papareact.com/8ix","title":"Pet allowed"}];
@@ -17,23 +19,22 @@ export default function Home() {
 
       <Header />
       <Banner />
-      
+
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          { exploreData.map((item) => (
-            <SmallCard 
-            key={item.img}
-            img={item.img} 
-            distance={item.distance} 
-            location={item.location} 
-            />
-          ))}
+            { exploreData.map((item) => (
+              <SmallCard 
+              key={item.img}
+              img={item.img} 
+              distance={item.distance} 
+              location={item.location} 
+              />
+            ))}
           </div>
-     
-
         </section>
+
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
@@ -41,8 +42,17 @@ export default function Home() {
               <MediumCard img={item.img} title={item.title} key={item.img} />
             ))}
           </div>
-
         </section>
+
+        <section>
+          <LargeCard 
+          img={banner2} 
+          title="The Greatest Outdoors"
+          description="Whislist curated by Airbnb." 
+          buttonText="Get Inspired"
+          />
+        </section>
+
       </main>
     </div>
   );
